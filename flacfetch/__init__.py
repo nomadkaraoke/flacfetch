fc22e04a13a70e431c51b5bac3e737e00b9d13d2
Andrew Beveridge <andrew@beveridge.uk>
2025-11-24 19:35:12 -0500

    Bump version to 0.3.3 - Test fixed unified workflow

--- flacfetch/__init__.py
+++ flacfetch/__init__.py
@@ -1,6 +1,6 @@
 """flacfetch - Search and download high-quality audio from multiple sources."""
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 __author__ = "Andrew Beveridge"
 __email__ = "andrew@beveridge.uk"
 
